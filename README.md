f2557c9920d6513e6d960288a4ea14e90985a799
Hagen10 <simon_hagerup@hotmail.com>
2025-02-04 19:36:41 +0100

    new way of setting up the globe as an object

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### Look into doing this zoomable Raster & Vector to project a worldmap (maybe not needed)
 ### Look at adding planes/satelittes/weather/skies
 ### The twilight/solar terminator seems a bit off, not sure why. The twilight +5 degrees comes from nowhere. Need to find a source for how far away it starts from the solar terminator.
+### It would be cool with a drag and measure distance between two points.
+### Greenland and Antarctica is an eye sore. Please make them white or something 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,83 @@
 import './style.css'
-import { createGlobe } from './globe'
+// import { createGlobe } from './globe'
+import { Globe, setupGlobe } from './globes'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div id="container"></div>
   </div>
 `
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav")!.style.width = "250px";
+}
 
-createGlobe(document.querySelector<HTMLElement>('#container')!)
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav")!.style.width = "0";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sideNav = document.getElementById("mySidenav");
+  const settingsButton = document.getElementById("button");
+  const closeButton = document.getElementById("closebtn");
+
+  if (closeButton) closeButton.addEventListener('click', closeNav);
+  if (settingsButton) settingsButton.addEventListener('click', openNav);
+
+  if (sideNav) {
+    sideNav.addEventListener('click', (event) => {
+      if (event.target instanceof HTMLAnchorElement) {
+        const link = event.target as HTMLAnchorElement;
+        const href = link.getAttribute('href')!;
+
+        switch (href) {
+          case '#about':
+            console.log("Chose About");
+            break;
+          case '#services':
+            console.log("Chose Services");
+            break;
+          case '#clients':
+            console.log("Chose Clients");
+            break;
+          case '#contact':
+            console.log("Chose Contact");
+            break;
+          case '#solarterminator':
+            if (globe.solarTerminator) {
+              console.log("Turning off Solar Terminator");
+
+              globe.setSolarTerminator(false);
+
+              // createGlobe(
+              //   document.querySelector<HTMLElement>('#container')!,
+              //   { solarTerminator: solarTerminator = false }
+              // )
+            }
+            else {
+              console.log("Turning on Solar Terminator");
+
+              globe.setSolarTerminator(true);
+
+              // createGlobe(
+              //   document.querySelector<HTMLElement>('#container')!,
+              //   { solarTerminator: solarTerminator = true }
+              // )
+            }
+            break;
+
+          default:
+            window.location.href = href;
+        }
+      }
+    });
+  }
+});
+
+// createGlobe(document.querySelector<HTMLElement>('#container')!, { solarTerminator: true })
+
+var globe = new Globe(document.querySelector<HTMLElement>('#container')!);
+
+setupGlobe(globe);
+
